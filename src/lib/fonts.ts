39bf8cafdf4fbae6d1a8ff1669b135d5a9e464bd
armangohari <arman.gohari.2001@gmail.com>
2024-05-20 10:21:26 +0330

    feat: add template code
    Added template code for the fonts.ts file.

---
+++ src/lib/fonts.ts
@@ -0,0 +1,36 @@
+import local from "next/font/local";
+
+export const exampleFont = local({
+  src: [
+    {
+      path: "../../public/assets/fonts/example-ultralight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/example-light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/example-regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/example-medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/example-bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/example-black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
